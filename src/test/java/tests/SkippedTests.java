4151acd38f7a1b51d5852169c007c8d65b719389
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-01 18:16:49 +0600

    added simple tests

---
+++ src/test/java/tests/SkippedTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void test0() {
+        assertTrue(false);
+    }
+    @Test
+    @Disabled("with some reason")
+    void test1() {
+        assertTrue(false);
+    }
+
+}
