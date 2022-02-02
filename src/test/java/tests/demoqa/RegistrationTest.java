45fdf95d7421487a567d8d90ce32ec8933d0a35b
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-02 20:06:51 +0600

    added steps

--- src/test/java/tests/demoqa/RegistrationTest.java
+++ src/test/java/tests/demoqa/RegistrationTest.java
@@ -8,33 +8,53 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationTest extends TestBase {
     @Test
     void practice() {
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        $("[alt=\"adplus-dvertising\"]").click();
-        $("#firstName").setValue("Bigaisha");
-        $("#lastName").setValue("Shalabayeva");
-        $("#userEmail").setValue("bigaisha@gmail.com");
-        $("#genterWrapper").$(byText("Female")).click();
-        $("#userNumber").setValue("7023998900");
-        $("#currentAddress").setValue("Samal-1-19-3");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("November");
-        $(".react-datepicker__year-select").selectOption("1991");
-        $("[aria-label$='November 5th, 1991']").click();
-        $("#subjectsInput").setValue("Eng");
-        $("#subjectsWrapper").$(byText("English")).click();
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-//        $("#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#state").scrollTo().click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
+        step("Открываем форму регистрации", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
 
+        step("Закрываем рекламный баннер", () -> {
+            $("[alt=\"adplus-dvertising\"]").click();
+        });
+        step("Вводим ФИО", () -> {
+            $("#firstName").setValue("Bigaisha");
+            $("#lastName").setValue("Shalabayeva");
+        });
+        step("Вводим почту, пол, номер телефона и адрес", () -> {
+            $("#userEmail").setValue("bigaisha@gmail.com");
+            $("#genterWrapper").$(byText("Female")).click();
+            $("#userNumber").setValue("7023998900");
+            $("#currentAddress").setValue("Samal-1-19-3");
+        });
+        step("Вводим дату рождения", () -> {
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("November");
+            $(".react-datepicker__year-select").selectOption("1991");
+            $("[aria-label$='November 5th, 1991']").click();
+        });
+        step("Выбираем увлечения", () -> {
+            $("#subjectsInput").setValue("Eng");
+            $("#subjectsWrapper").$(byText("English")).click();
+            $("#hobbiesWrapper").$(byText("Reading")).click();
+        });
+
+//        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        step("Выбираем штат и город", () -> {
+            $("#state").scrollTo().click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+        });
+        step("Подтверждаем форму", () -> {
+             $("#submit").click();
+        });
+
+        step("Проверяем введенные данные", () -> {
         $(".table").shouldHave(
                 text("Bigaisha Shalabayeva"), text("bigaisha@gmail.com"),
                 text("Female"), text("7023998900"),
@@ -42,7 +62,8 @@ public class RegistrationTest extends TestBase {
                 text("Reading"),
 //                text("1.png"),
                 text("Samal-1-19-3"),text("NCR Delhi")
-        );
+            );
+        });
     }
 }
 
