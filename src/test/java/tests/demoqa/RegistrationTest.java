b608d26cca84aed0af173f7daf0472943a63de49
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-02 19:35:02 +0600

    added selenide listener

--- src/test/java/tests/demoqa/RegistrationTest.java
+++ src/test/java/tests/demoqa/RegistrationTest.java
@@ -1,5 +1,7 @@
 package tests.demoqa;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -10,7 +12,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class RegistrationTest extends TestBase {
     @Test
     void practice() {
-
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("[alt=\"adplus-dvertising\"]").click();
