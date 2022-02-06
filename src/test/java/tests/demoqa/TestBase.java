3f0bfbcdf355678bb3f29565fcb860c320b32c45
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-06 13:26:35 +0600

    added system properties

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -18,13 +18,22 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+        String user = System.getProperty("user","user1");
+        String password = System.getProperty("password", "1234");
+        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://" + user + ":" + password + "@" + url;
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "91");
+
     }
 
     @AfterEach
