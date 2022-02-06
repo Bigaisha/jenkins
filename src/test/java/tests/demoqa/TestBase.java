e508f437488d3f0d8a634de66e35ae14a46fe6f9
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-02-06 13:36:38 +0600

    added system properties

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -19,21 +19,18 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "91");
 
         String user = System.getProperty("user","user1");
         String password = System.getProperty("password", "1234");
         String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub");
         Configuration.remote = "https://" + user + ":" + password + "@" + url;
 
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("version", "91");
-
     }
 
     @AfterEach
